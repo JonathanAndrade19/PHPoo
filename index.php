04813544398947da143fe2304fbe3bd2fafed736
jonathanAndrade19 <jonathanjampamb@gmail.com>
2021-01-11 15:47:51 -0300

    Aula 03 Configurando Visibilidade de Atributos e Métodos

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
     <title>PHP OO</title>
 </head>
 <body>
-    
+<?php
+    require_once 'Caneta.php';
+    $cl = new Caneta;
+    $cl->modelo = "Bic Cristal";
+    $cl->cor = "Azul";
+    // $cl->ponta = 0.5;
+    $cl->rabiscar();
+    $cl->tampar();
+    var_dump($cl);
+
+?>    
 </body>
 </html>
