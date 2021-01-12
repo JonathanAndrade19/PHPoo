17ada8c934a29fbe49e0f2df6b7e9147c233d3f6
jonathanAndrade19 <jonathanjampamb@gmail.com>
2021-01-12 17:34:50 -0300

    Exemplo Prático em PHPoo ex01 finalizado!

--- Banco/ContaBanco.php
+++ Banco/ContaBanco.php
@@ -11,33 +11,34 @@ class ContaBanco{
     public function abirConta($t){
         $this->setTipo($t);
         $this->setStatus(true);
-
-        if ( $t == "CC" ){
+        if ($t == "CC"){
             $this->setSaldo(50);
-        } elseif ( $t == "CP" ){
-            $this->setSaldo(1500);
+        } elseif ($t == "CP"){
+            $this->setSaldo(150);
         }
     } 
     public function fecharConta(){
-        if ( $this->getSaldo() > 0 ){
+        if ($this->getSaldo() > 0){
             echo "<p>A Conta ainda tem Saldo, não e possivel finalizala!</p>";
-        } elseif ( $this->getSaldo() < 0 ){
+        } elseif ($this->getSaldo() < 0){
             echo "<p>A Conta Está em Debito. Impossivel Finalizala!</p>";
         } else {
             $this->setStatus(false);
         }
     }
     public function depositar($v){
-        if ( $this->getStatus() ){
-            $this->setSaldo( $this->getSaldo() + $v );
-        } else {
+        if($this->getStatus()){
+            $this->setSaldo($this->getSaldo() + $v);
+            echo "<p>Depósito de R$: $v na conta de ". $this->getNomeDono() ."</p>";
+        }else{
             echo "<p>Conta Fechada com Sucesso, Não e mais possivel Depositar!</p>";
         }
     }
     public function sacar($v){
         if($this->getStatus()){
-            if ( $this->getSaldo() > $v ){
-                $this->setSaldo( $this->getSaldo() - $v );
+            if ($this->getSaldo() > $v){
+                $this->setSaldo($this->getSaldo() - $v);
+                echo "<p>Saque de $v Autorizado na conta: ".$this->getNomeDono()."</p>";
             } else {
                 echo "<p>Saldo Insuficiente para Sacar.</p>";
             }
@@ -46,14 +47,15 @@ class ContaBanco{
         }
     }
     public function pagarMensal(){
-        if ( $this->getTipo() == "CC" ){
+        if( $this->getTipo() == "CC" ){
             $v = 12;
-        } elseif ( $this->getTipo() == "CP" ){
+        }elseif($this->getTipo() == "CP"){
             $v = 20;
         }
 
         if($this->getStatus()){
             $this->setSaldo( $this->getSaldo() - $v );
+            echo "<p>Mensalidade de R$: $v debitado na conta de ".$this->getNomeDono()."</p>";
         } else {
             echo "<p>Problema Com a conta, Não e Possivel Cobrar a Anuidade.</p>";
         }
--- Banco/index.php
+++ Banco/index.php
@@ -6,10 +6,33 @@
     <title>Conta Banco</title>
 </head>
 <body>
+<pre>
     <?php
     require_once 'ContaBanco.php';
+
     $Conta01 = new ContaBanco();
     $Conta02 = new ContaBanco();
+
+    $Conta01->abirConta("CC");
+    $Conta01->setNomeDono("Lídia Araújo");
+    $Conta01->setNumeroConta("220158-9");
+
+    $Conta02->abirConta("CP");
+    $Conta02->setNomeDono("Jonathan Moreira");
+    $Conta02->setNumeroConta("221269-0");
+
+    // $Conta01->depositar(122);
+    // $Conta02->depositar(87);
+
+    // $Conta01->sacar(65);
+    // $Conta01->sacar(20);
+
+    // $Conta01->pagarMensal();
+    // $Conta01->pagarMensal();
+
+    print_r($Conta01);
+    print_r($Conta02);
     ?>
+</pre>
 </body>
 </html>
